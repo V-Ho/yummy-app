7bd8de6bedff5dbb96830d10bcd4da4a5cc69c7c
Vanessa Ho <vanessa.lg.ho@gmail.com>
2018-09-02 22:44:03 +0200

    async await axios request to recipes page

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
-import num from './test'
-const x = 23
-console.log(`I imported ${num} from another module called test.js. Variable x is ${x}`)
+import axios from 'axios'
+
+async function getResults (query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/'
+  const key = '2239d7003b5947bf25bb80eff1082ca7'
+  try {
+    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`)
+    const recipes = res.data.recipes
+    console.log(recipes)
+  } catch (error) {
+    console.log(error)
+  }
+}
+getResults('avocado')
+// http://food2fork.com/api/searc
+// API Key 2239d7003b5947bf25bb80eff1082ca7
