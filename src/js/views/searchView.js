71a3caaf65e41a4671bd4c7f265d0202ab743f0c
Vanessa Ho <vanessa.lg.ho@gmail.com>
2018-09-12 17:10:20 +0200

    limit result titles to 17 characters

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -11,25 +11,42 @@ export const clearResults = () => {
   elements.searchResList.innerHTML = '' // clears all <li> elements
 }
 
+// // Limit recipe title displayed to 17 letters
+// const limitRecTitle = (title) => {
+//   // put title into array of words
+//   // reduce method to count number of letters
+// }
+
+const limitRecTitle = (title) => {
+  const result = title.split(' ').reduce((acc, currVal) => {
+    if (acc.length + currVal.length < 17) {
+      return `${acc} ${currVal}`
+    }
+    return `${acc}`
+  }, '')
+  return `${result}...`
+}
+
 const renderRecipe = recipe => {
   const markup = `
   <li>
-                    <a class="results__link" href="#${recipe.recipe_id}">
-                        <figure class="results__fig">
-                            <img src="${recipe.image_url}" alt="${recipe.title}">
-                        </figure>
-                        <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
-                            <p class="results__author">T${recipe.publisher}</p>
-                        </div>
-                    </a>
-                </li>
+      <a class="results__link" href="#${recipe.recipe_id}">
+          <figure class="results__fig">
+              <img src="${recipe.image_url}" alt="${recipe.title}">
+          </figure>
+          <div class="results__data">
+              <h4 class="results__name">${limitRecTitle(recipe.title)}</h4>
+              <p class="results__author">T${recipe.publisher}</p>
+          </div>
+      </a>
+  </li>
   `
   // display results list in ascending order
   elements.searchResList.insertAdjacentHTML('beforeend', markup)
 }
 
 export const renderResults = recipes => {
+  console.log(recipes)
   // cb function, renders each recipe
   recipes.forEach(renderRecipe)
 }
